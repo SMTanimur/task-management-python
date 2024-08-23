4b4c854242b622a468357f17c1332e1466c2a633
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-08-23 22:09:19 +0600

    chore: Update FastAPI application structure and dependencies

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,13 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,APIRouter
+import uvicorn
 from pydantic import BaseModel
-app = FastAPI()
+from app.config import db
+from fastapi.middleware.cors import CORSMiddleware
+from app.config import db
 
-app = FastAPI(title="Tast management", version="0.1.0")
+origins= [
+    "http://localhost:3000"
+]
 
+def init_app():
+    db.init()
 
-@app.get("/", tags=["Root"])
-async def root() -> dict:
-    # print(settings.DATABASE_URL)
-    return {"message": "Assalamua Alaikum, Welcome to the Task management API"}
+    app = FastAPI(
+        title= "Tast management",
+        description= "A task management app",
+        version= "0.1.0"
+    )
 
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
 
+    @app.on_event("startup")
+    async def starup():
+        await db.create_all()
+    
+    @app.on_event("shutdown")
+    async def shutdown():
+        await db.close()
+   
+    return app
+
+app = init_app()
+
+@app.get('/')
+def root():
+      return {"message": "Welcome to the Task Management API"}
+
+def start():
+    """ Alhumdullilah, Launched with 'poetry run start' at root level """
+    uvicorn.run("app.main:app", host="localhost", port=8888, reload=True)
