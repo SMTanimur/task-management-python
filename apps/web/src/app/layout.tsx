3a95d703a520fcf23a041b7cdf440544c3d51519
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-09-13 15:33:54 +0600

    chore: Update .prettierrc and tailwind.config.ts

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { ThemeProvider } from "next-themes";
+import { ThemeDataProvider } from "@/context";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ThemeDataProvider>{children}</ThemeDataProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
